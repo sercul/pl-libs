82c0e2a0663a08a8ef900be1e150f9328abfc460
Maris Graudins <maris.graudins91@gmail.com>
2014-12-30 15:56:27 +0200

    Remove hardcoded line from Yii core lib

--- yii/framework/web/CAssetManager.php
+++ yii/framework/web/CAssetManager.php
@@ -151,9 +151,6 @@ class CAssetManager extends CApplicationComponent
 	 */
 	public function publish($path,$hashByName=false,$level=-1,$forceCopy=false)
 	{
-		// added by TSS:
-		if (YII_DEBUG) $forceCopy = true;
-		
 		if(isset($this->_published[$path]))
 			return $this->_published[$path];
 		else if(($src=realpath($path))!==false)
